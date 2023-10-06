871240696e8700b8d82100d205ada577b2b59cdb
PRAKASH <prakashdp411@gmail.com>
2023-10-06 21:14:58 +0530

    type casting

---
+++ Practice_day_1.py
@@ -0,0 +1,51 @@
+print("jai balayya")
+
+# Type casting ( converstion of int=>float=>string )
+
+# int==>float
+
+x = 20 
+y = float(x)
+print(type(y),y)
+
+
+# float==>int
+
+x = 50.5
+
+y = int(x)
+print(type(y),y)
+
+
+
+# string==>int
+
+x = "500"
+y = int(x)
+print(type(y),y)
+
+
+# string==>float
+
+x = "600"
+y = float(x)
+print(type(y),y)
+
+
+#int==>string
+
+x = 100
+y = str(x)
+print(type(y),y)
+
+
+
+
+# string+string
+
+x = "cri"
+y = "cket"
+
+print(x+y)
+
+      
