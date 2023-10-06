790f89a6c8259fbd8aa2f60e88af02e29ad7a283
PRAKASH <prakashdp411@gmail.com>
2023-10-06 16:40:58 +0530

    first commit

---
+++ test 1.py
@@ -0,0 +1,20 @@
+print("jai balayya")
+
+
+# Definition
+# Python is dynammic,object oriented,scripting language,programming language and high
+#      high level programming language
+
+# variables
+
+x = 10   #( int)
+y = 20.1 #(float)
+z = ("fasak") #(string)
+
+print("X+Y",x+y)
+print("X*Y",x*y)
+print("x/y",x/y)
+
+
+
+
