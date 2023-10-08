37417a188d3dd86903c175c8849d48096ebe0971
PRAKASH <prakashdp411@gmail.com>
2023-10-08 10:15:31 +0530

    OPERATORS

---
+++ practice day 2.py
@@ -0,0 +1,185 @@
+print("jai balayya")
+print("jai balayya")
+
+                          #-----------
+                          # OPERATORS
+                          #-----------
+
+
+# OPERATORS IS AGAIN DIVIDED INTO FOLLOWING TYPES
+"""
+        (1) ARITHIMETIC OPERATORS
+        (2) EQUAL OPERATORS(or)comparative operators
+        (3) RELATIONAL OPERATORS
+        (4) MEMBERSHIP OPERATORS
+        (5) IDENTIFY OPERATORS
+        (6) INCREMENTAL OPERATORS
+"""
+"""
+# (1) ARTHIMETIC OPERATORS( +, -, *, /, //, % )
+x = 50
+y = 20
+# ADDITION
+print(x+y)
+"convertion of int===>str"
+x = str(x)
+print(type(x),x)
+y = str(y)
+print(type(y),y)
+
+print(x+y)
+"convertion of str===>int"
+x = int(x)
+y = int(y)
+# SUBRACTION
+X = 50
+Y = 20
+
+print(x-y)
+# MULTIPLICATION
+X = 50
+Y = 20
+print(x*y)
+
+#DIVISION
+
+X = 50
+Y = 20
+
+print(x/y)
+
+#FLOOR DIVISION
+
+X = 50
+Y = 20
+"""
+"""
+print(x//y)
+
+#$
+
+x = 100
+y = 50
+
+print(x%y)
+"""
+"""
+# (2)EQUAL OPERATORS(== ,!=)
+
+x = 50
+y = 100
+
+print(x==y)
+print(x!=y)
+"""
+"""
+# RELATIONAL OPERATORS( >, <, >=, <= )
+
+x = 30
+y = 60
+
+print(x>y)
+print(x<y)
+print(x>=y)
+print(x<=y)
+"""
+"""
+
+# MEMBERSHIP OPERATORS ( IN, NOT IN )
+
+INDIA = "DHONI,VIRAT,ROHIT"
+
+print("DHONI" in INDIA)
+print("BABAR AZAM" not in INDIA)
+print("BABAR AZAM" is INDIA)
+"""
+"""
+#IDENTIFY OPERATORS ( IS , IS NOT )
+
+DHONI = 7
+VIRAT = 18
+
+print(id(DHONI))#1321257992624
+print(id(VIRAT))#2706716427024
+
+print(DHONI is VIRAT)
+print(DHONI is not VIRAT)
+"""
+
+#INCREMENTAL OPERATORS( +=, -=, *=, /= )
+
+x = "pra"
+
+x = x+"kash"
+
+print(x)
+
+y = 100
+
+y = y-5
+
+print(y)
+
+z = 10
+z = z*10
+
+print(z)
+
+h = 1
+h = 1/2
+
+print(h)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+ 
