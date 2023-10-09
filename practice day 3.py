0584c014699afbc13c2bad353b79e19cb5669a73
PRAKASH <prakashdp411@gmail.com>
2023-10-09 20:49:51 +0530

    STRING MANIPLATIONS AND INPUT

---
+++ practice day 3.py
@@ -0,0 +1,163 @@
+print("prakash")
+print("prakash")
+
+                #STRING MANIPULATIONS
+"""
+
+#UPPER CASE
+
+x = "dhoni"
+z = x.upper()
+print(z)#DHONI
+
+#LOWER CASE
+
+k = "VIRAT"
+y = k.lower()
+print(y)#virat
+
+print(k.isupper())#TRUE
+print(x.isupper())#FALSE
+print(k.islower())#FALSE
+"""
+"""
+#TITLE
+
+x = "prakash"
+
+y = x.title()
+
+print(y)#Prakash
+
+xx = "virat kohli"
+
+yy = xx.title()
+
+print(yy)#Virat Kohli
+"""
+"""
+#CAPTALIZE
+
+x = "aKHIL"
+y = x.capitalize()
+print(y)#Akhil
+"""
+"""
+#REPLACE
+
+x = "MAHENDRA SINGH VIRAT"
+y =x.replace("VIRAT","DHONI")
+print(y)#MAHENDRA SINGH DHONI
+"""
+"""
+#SPLIT
+
+x = "POIDHONI"
+y = x.split("P""O""I")
+print(y)
+"""
+"""
+#JION
+
+x = "PC "
+print("O".join(x))#POCO
+"""
+"""
+#INPUT
+
+x = float(input("enter the x value:-"))#10
+y = int(input("enter the y value:-"))#20
+
+print(x+y)#30.0
+"""
+"""
+x = float(input("enter the x value:-"))#10
+y = int(input("enter the y value:-"))#30
+
+print(x*y)#300.0
+"""
+"""
+x = input("enter the x value:")#DHONI
+y = input("enter the y value:")#7
+
+print(x+y)#DHONI7
+"""
+"""
+x = int(input("enter the x value:"))
+y = int(input("enter the y value:"))
+
+
+print(x+y)#90
+print(x-y)#10
+print(x*y)#2000
+print(x/y)#1.25
+print(type(x))#<class 'int'>
+print(x//y)#1
+print(x!=y)#True
+print(x<y)#False
+print(x is not y)#True
+"""
+
+x = input("enter the x value:")
+y = input("enter the y value:")
+
+x = x.upper()
+y = y.lower()
+print(x)#EGG
+print(y)#roll
+
+print(x+y)#EGGroll
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
