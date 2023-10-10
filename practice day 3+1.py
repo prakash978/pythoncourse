b10b5624d1a8134c3cdfad995e09489d8a93408c
PRAKASH <prakashdp411@gmail.com>
2023-10-10 22:02:01 +0530

    conditions

---
+++ practice day 3+1.py
@@ -0,0 +1,77 @@
+# FLOW CONTROLS OR CONDITIONAL STATEMENTS
+
+print("AKHIL")
+print("AKHIL")
+
+"""
+#IF CONDITION
+
+x =int(input("enter the x value:"))#x=40
+if x>10:
+
+    print("x is greater than 10",)
+    
+"""
+"""
+#IF AND ELSE CONDITION
+
+x =int(input("enter the x value:"))#30
+y =int(input("enter the y value:"))#20
+
+if x==y:
+    print(" x and y are same")
+
+else:
+    print("x is greater than y")
+"""
+"""
+#IF,ELIF AND ELSE
+
+x =float(input("enter the x value:"))#30
+y =float(input("enter the y value:"))#40
+
+if x==y:
+    print("x eual to y")
+elif x/y:
+    print("x is divided by y")
+else:
+    priny("x and y not divided ")
+"""
+"""
+x =int(input("enter the x value:"))#10
+if x%2!=0:
+    print("odd number x")
+else:
+    print("even number x")
+"""
+x =input("enter the x value:")#syed
+y =input("enter the y value:")#AKHIL
+
+if x.isupper():
+    print("upper charecters ")
+
+else:
+    print("lower charecters")
+    
+if y.isupper():
+    print("upper charecters")
+else:
+    print("lower charecters")
+    
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+
+
+
