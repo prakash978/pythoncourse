2b1ceb6d9b2e83daa732bf2ec11bb166cdb743c9
PRAKASH <prakashdp411@gmail.com>
2023-10-11 22:00:57 +0530

    INDEXING

---
+++ practice day 5.py
@@ -0,0 +1,44 @@
+print("Prakash")
+print("Prakash")
+
+#INDEXING
+
+#SPECIFIED
+"""
+x = "ROHIT-SHARMA"
+y = "98#76%5"
+print(x[5])#-
+print(y[4])#6
+print(y[2])##
+"""
+
+#START:STOP
+"""
+x = "DFGDHONITYU"
+y = "FTGDVIRATYUI"
+
+print(x[3:8])#DHONI
+print(y[4:9])#VIRAT
+"""
+
+x = "4987JKK"
+
+print(x[ :4])#4987
+print(x[4: ])#JKK
+print(x[ : ])#4987JKK
+
+#START:STOP:STEP
+
+x = "RTDGHTOGNDIK GIGSK HGGOHAIT"
+y = "SD7G GSFIH FOGNK JILNYOTH7D"
+print(x[2:27:2])#DHONI IS GOAT
+print(y[:1:-2])#DHONI NO IS 7
+print(len(x))
+
+
+
+
+
+
+
+
