d65a9a7d4e8c9b48589c31ad11deacaf39b9408a
PRAKASH <prakashdp411@gmail.com>
2023-10-12 19:36:30 +0530

    LOOPS AND RANGE

---
+++ practice day 6.py
@@ -0,0 +1,147 @@
+print("PRAKASH")
+print("PRAKASH")
+
+
+#LOOPS AND RANGE
+
+#RANGE
+"""
+print(range(10))#range(0, 10)
+print(range(0))#range(0, 0)
+"""
+"""
+x = 7
+
+print(list(range(x)))#[0, 1, 2, 3, 4, 5, 6]
+print(list(range(2,5)))#[2, 3, 4]
+print(list(range(2,7,2)))#[2, 4, 6]
+"""
+
+#LOOPS
+
+#FOR
+"""
+x = input("enter the x:")
+"""
+"""
+for x in range(10):
+    print(x)
+
+
+for x in range(10):
+    print(x,end="")#0123456789
+"""
+"""
+for x in range(4,10,3):
+    print(x,end="")
+    print(x)#77
+"""
+
+#BREAK
+"""
+for x in range(2,7):
+    if x == 5:
+        break
+    print(x)#
+
+for x in "DHONII7":
+    if x == "7":
+        break
+    print(x)
+"""
+#ELSE
+"""
+for x in range(10):
+    print(x)
+else:
+    print("fully excuted")
+
+for x in range(7):
+    if x == 5:
+        break
+    print(x)
+else:
+    print("fully not excuted")
+"""
+
+#CONTINUE
+"""
+for x in range(16):
+    if x == 5:
+        continue
+    print(x)
+"""
+"""
+for x in "PRAKASH":
+    if x == "S":
+        continue
+    print(x,end="")
+"""
+"""
+for x in range(10):
+    if x//3:
+        continue
+    print(x,end="")#012
+"""
+"""
+for x in range(5):
+    if x*3:
+        continue
+    print(x,end="")#0
+"""    
+#WHILE
+"""
+x = 0
+
+while x<10:
+    x = x+1
+    print(x)
+"""
+x = 10
+
+while x/1:
+    x = x-1
+    print(x)
+
+
+
+
+  
+
+
+        
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
