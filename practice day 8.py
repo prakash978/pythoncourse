b382cf230c905f58b872681e2c8d554441130c2b
PRAKASH <prakashdp411@gmail.com>
2023-10-16 19:00:38 +0530

    DATA STRUCTURES== TUPLE & DICT

---
+++ practice day 8.py
@@ -0,0 +1,66 @@
+#DATA STRUCTURES
+
+#TUPLE
+
+#COUNT
+"""
+x = input("enter the x value:")#prakash
+
+for xx in x:
+    print(xx.count("p"))#1
+
+"""
+"""
+#SORTED
+
+x = input("enter the x value:")#4320
+
+print(sorted(x))#['0', '2', '3', '4']
+"""
+#INDEXING
+"""
+x = (4,6,7,8)
+print(x[2])#7
+
+"""
+#DICT
+
+#ADD
+
+x = {"dhoni":7,"virat":17}
+x["rohit"]=45
+print(x)#{'dhoni': 7, 'virat': 17, 'rohit': 45}
+
+print(x['dhoni'])#7
+
+#UPDATE
+
+
+y = {5:8,7:5}
+x.update(y)
+print(x)#{'dhoni': 7, 'virat': 17, 'rohit': 45, 5: 8, 7: 5}
+
+
+for key,value in x.items():
+    print('key:',value)
+#POP
+x.pop('dhoni')
+print(x)
+
+#popitem
+x.popitem()
+print(x)
+    
+
+    
+
+  
+
+
+
+
+
+
+
+    
+
