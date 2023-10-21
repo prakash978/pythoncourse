5b815dfb6d461a790f91215ee067df9daa30aab9
PRAKASH <prakashdp411@gmail.com>
2023-10-21 18:13:18 +0530

    sum of n elements

--- Backlog 1.py
+++ Backlog 1.py
@@ -1,20 +1,11 @@
-#TO SUM 'n' NUMBER OF ELEMENTS
-"""
+
+  #TO SUM 'n' NUMBER OF ELEMENTS
+
 def function (*arg):
-    return(1,2,3,4,5)
-x = function(1,2,3,4,5)
+    return sum(range(10))
+x = function(10)
 
 print(x)
-print(sum(x))#15
-"""
-def function(**kwargs):
-    return kwargs
-
-x = function(x=50,y=50,z=20)
-print(x)
-print(x.values())
-print(sum(x.values()))#120
-
 
 
 
